d28749096f100697d5bdeac0e18db1be5679fa0c
Hack Reactor Students <accounts@hackreactor.com>
2016-12-12 17:38:27 -0800

    Working on parse (incomplete)

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -1,6 +1,8 @@
 // test cases are described in fixtures.js
 describe('parseJSON', function() {
 
+
+
   it('should match the result of calling JSON.parse', function() {
     parseableStrings.forEach(function(test) {
       var result = parseJSON(test);
@@ -8,9 +10,46 @@ describe('parseJSON', function() {
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      expect(equality).to.equal(true);
     });
   });
+
+  it('should match JSON.parse', function() {
+      
+    var result = parseJSON('[]');
+    var expected = JSON.parse('[]');
+    var equality = _.isEqual(result, expected);
+    expect(equality).to.equal(true);
+
+  });
+
+
+
+/*
+ '[]',
+  '{"foo": ""}',
+  '{}',
+  '{"foo": "bar"}',
+  '["one", "two"]',
+  '{"a": "b", "c": "d"}',
+  '[null,false,true]',
+  '{"foo": true, "bar": false, "baz": null}',
+  '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
+  '{"boolean, true": true, "boolean, false": false, "null": null }',
+
+  // basic nesting
+  '{"a":{"b":"c"}}',
+  '{"a":["b", "c"]}',
+  '[{"a":"b"}, {"c":"d"}]',
+  '{"a":[],"c": {}, "b": true}',
+  '[[[["foo"]]]]',
+*/
+
+
+
+
+
+  
 
   it('should throw an error for invalid stringified JSON', function() {
     unparseableStrings.forEach(function(test) {
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,68 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  // your code goes here
+//some function to determine the next character
+
+
+
+// ARRAY FUNCTION
+var parseJSONArray = function(json, startingIndex, result) {
+  var result = [];
+  for (var i = 0; i < json.length; i++) {
+    if (json[i] === "]") {
+      var endingIndex = i;
+    }
+  }
+  var stringArray = json.slice(startingIndex+1, endingIndex);
+  var unparsedArray = stringArray.split(", ");
+  _.each(unparsedArray, function(element){
+    result.push(parseJSON(element));
+  });
+  return result;
+};
+
+
+// OBJECT FUNCTION
+var parseJSONObject = function(json, startingIndex, result) {
+
+};
+
+// STRING FUNCTION
+var parseJSONString = function(json, startingIndex) {
+  for (var i = 1; i < json.length; i++) {
+    if (json[i] === '"') {
+      var endingIndex = i;
+    }
+  }
+  return json.slice(startingIndex + 1, endingIndex);
+};
+
+var parseJSON = function(json, startingIndex, result) {
+  if (!result){
+    if (json[0] === "[") {
+      var result = [];
+    
+    } else if (json[0] === "{") {
+      var result = {};
+    
+    }
+  }
+
+
+
+  for (var currentChar = 0; currentChar < json.length; currentChar++) {
+  // set up functionality for different data types
+  // string
+    if (currentChar === '"') {
+      return parseJSONString(json, currentChar);
+    }
+  // number
+  // boolean
+  // object
+  // array
+    if (currentChar === "["){
+      return parseJSONArray(json, currentChar);
+    }
+  }
+  // null
 };
